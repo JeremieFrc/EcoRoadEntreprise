5b600bd4f1f65a43d285ee6de17f643975512613
jeremieFrc <ferlayjeremie@gmail.com>
2019-01-21 10:45:52 +0100

    begin back save V0.01

---
+++ src/main/java/com/societe/projet/EcoRoadEntrepriseApplication.java
@@ -0,0 +1,15 @@
+package com.societe.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoRoadEntrepriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoRoadEntrepriseApplication.class, args);
+		System.out.println("test ok");
+	}
+
+}
+
